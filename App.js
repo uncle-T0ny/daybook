754cb3c46fff7c8e5e80a6ccb54a9bbb88833031
aromankov <anton.romankov@ambisafe.co>
2018-07-15 17:39:35 +0300

    added react-native-material-ui and react-native-vector-icons

--- App.js
+++ App.js
@@ -1,6 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import { Provider } from 'react-redux';
+import {
+  RadioButton,
+  Avatar,
+  Divider,
+  ListItem,
+  Subheader,
+  Toolbar,
+  ActionButton,
+  Card,
+  IconToggle,
+  Badge,
+  Icon,
+  BottomNavigation,
+  BottomNavigationAction
+} from 'react-native-material-ui';
 
 import { configureStore } from './src/store.js';
 import { bindAppActions, appReduxActions } from './src/reducers/appReducer.js';
@@ -10,6 +25,14 @@ const store = configureStore();
 bindAppActions(store.dispatch);
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: false,
+      navIndex: 0
+    }
+  }
+
   componentWillMount() {
     appReduxActions.appStarted();
   }
@@ -17,9 +40,68 @@ export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <View style={styles.container}>
-          <Text>Here is our Daybook App!</Text>
-        </View>
+          {/*<Toolbar*/}
+            {/*leftElement="menu"*/}
+            {/*centerElement="Searchable"*/}
+            {/*searchable={{*/}
+              {/*autoFocus: true,*/}
+              {/*placeholder: 'Search',*/}
+            {/*}}*/}
+            {/*rightElement={{*/}
+              {/*menu: {*/}
+                {/*icon: "more-vert",*/}
+                {/*labels: ["item 1", "item 2"]*/}
+              {/*}*/}
+            {/*}}*/}
+            {/*onRightElementPress={ (label) => { console.log(label) }}*/}
+          {/*/>*/}
+          {/*<ActionButton /> // default with icon (default icon is +)*/}
+          {/*<ActionButton icon="done" /> // with done icon*/}
+          {/*<Text>Here is our Daybook App!</Text>*/}
+          {/*<RadioButton*/}
+            {/*label="Unchecked"*/}
+            {/*checked={false}*/}
+            {/*value="Value"*/}
+            {/*onCheck={(checked) => this.setState({ checked })}*/}
+          {/*/>*/}
+          {/*<Divider/>*/}
+          {/*<Avatar icon="person" iconColor="blue" />*/}
+          {/*<Divider/>*/}
+          {/*<ListItem*/}
+            {/*divider*/}
+            {/*centerElement={{*/}
+              {/*primaryText: 'Primary text',*/}
+            {/*}}*/}
+            {/*onPress={() => {}}*/}
+          {/*/>*/}
+          {/*<Subheader text="Subheader text" />*/}
+
+        <BottomNavigation active={this.state.active} hidden={false}>
+          <BottomNavigation.Action
+            key="today"
+            icon="today"
+            label="Today"
+            onPress={() => this.setState({ active: 'today' })}
+          />
+          <BottomNavigation.Action
+            key="people"
+            icon="people"
+            label="People"
+            onPress={() => this.setState({ active: 'people' })}
+          />
+          <BottomNavigation.Action
+            key="bookmark-border"
+            icon="bookmark-border"
+            label="Bookmark"
+            onPress={() => this.setState({ active: 'bookmark-border' })}
+          />
+          <BottomNavigation.Action
+            key="settings"
+            icon="settings"
+            label="Settings"
+            onPress={() => this.setState({ active: 'settings' })}
+          />
+        </BottomNavigation>
       </Provider>
     );
   }
