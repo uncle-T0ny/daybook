6caac97f53c6b409f23be3f81d4e6cf6b62542df
Alexander Shirokov <alexandershirokov@MacBook-Pro-Alexander.local>
2018-07-16 23:14:11 +0300

    BottomNavigantion fixed + gitignore changed

--- App.js
+++ App.js
@@ -28,7 +28,7 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      active: false,
+      active: 'today',
       navIndex: 0
     }
   }
@@ -40,68 +40,36 @@ export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-          {/*<Toolbar*/}
-            {/*leftElement="menu"*/}
-            {/*centerElement="Searchable"*/}
-            {/*searchable={{*/}
-              {/*autoFocus: true,*/}
-              {/*placeholder: 'Search',*/}
-            {/*}}*/}
-            {/*rightElement={{*/}
-              {/*menu: {*/}
-                {/*icon: "more-vert",*/}
-                {/*labels: ["item 1", "item 2"]*/}
-              {/*}*/}
-            {/*}}*/}
-            {/*onRightElementPress={ (label) => { console.log(label) }}*/}
-          {/*/>*/}
-          {/*<ActionButton /> // default with icon (default icon is +)*/}
-          {/*<ActionButton icon="done" /> // with done icon*/}
-          {/*<Text>Here is our Daybook App!</Text>*/}
-          {/*<RadioButton*/}
-            {/*label="Unchecked"*/}
-            {/*checked={false}*/}
-            {/*value="Value"*/}
-            {/*onCheck={(checked) => this.setState({ checked })}*/}
-          {/*/>*/}
-          {/*<Divider/>*/}
-          {/*<Avatar icon="person" iconColor="blue" />*/}
-          {/*<Divider/>*/}
-          {/*<ListItem*/}
-            {/*divider*/}
-            {/*centerElement={{*/}
-              {/*primaryText: 'Primary text',*/}
-            {/*}}*/}
-            {/*onPress={() => {}}*/}
-          {/*/>*/}
-          {/*<Subheader text="Subheader text" />*/}
-
-        <BottomNavigation active={this.state.active} hidden={false}>
-          <BottomNavigation.Action
-            key="today"
-            icon="today"
-            label="Today"
-            onPress={() => this.setState({ active: 'today' })}
-          />
-          <BottomNavigation.Action
-            key="people"
-            icon="people"
-            label="People"
-            onPress={() => this.setState({ active: 'people' })}
-          />
-          <BottomNavigation.Action
-            key="bookmark-border"
-            icon="bookmark-border"
-            label="Bookmark"
-            onPress={() => this.setState({ active: 'bookmark-border' })}
-          />
-          <BottomNavigation.Action
-            key="settings"
-            icon="settings"
-            label="Settings"
-            onPress={() => this.setState({ active: 'settings' })}
-          />
-        </BottomNavigation>
+        <View style={{flex:1}}>
+            <View style={{flex: 1}}>
+            </View>
+            <BottomNavigation active={this.state.active} hidden={false} style={{flex: 1}}>
+              <BottomNavigation.Action
+                key="today"
+                icon="assignment"
+                label="Today"
+                onPress={() => this.setState({ active: 'today' })}
+              />
+              <BottomNavigation.Action
+                key="subjects"
+                icon="book"
+                label="Subjects"
+                onPress={() => this.setState({ active: 'subjects' })}
+              />
+              <BottomNavigation.Action
+                key="teachers"
+                icon="school"
+                label="Teachers"
+                onPress={() => this.setState({ active: 'teachers' })}
+              />
+              <BottomNavigation.Action
+                key="settings"
+                icon="settings"
+                label="Settings"
+                onPress={() => this.setState({ active: 'settings' })}
+              />
+            </BottomNavigation>
+        </View>
       </Provider>
     );
   }
